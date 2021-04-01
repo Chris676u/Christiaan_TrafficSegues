9011ef8e6d8832787f5034f442516ea0feaa912d
Christian Severin <severinchristian25@gmail.com>
2021-04-01 11:09:18 -0400

    Initial Commit

---
+++ Christiaan_TrafficSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Christiaan_TrafficSegues
+//
+//  Created by Christian Severin on 4/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
